da65ec312bd014cb5b594c74ce2324d69bde953f
alerner1 <alyssplerner@gmail.com>
2020-08-31 17:01:02 -0400

    add instruments

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,35 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: :show
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :age, :title))
+    
+    if @artist.valid?
+      @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
-Artist.create(name: "Michael Jackson", age: rand(50), title: "King of Pop")
-Artist.create(name: "Stevie Wonder", age: rand(50), title: "Little Stevie Wonder")
-Artist.create(name: "Elvis Presley", age: rand(50), title: "the King")
-Artist.create(name: "Whitney Houston", age: rand(50), title: "The Voice")
-Artist.create(name: "Beyoncé", age: rand(50), title: "Destiny's Child")
+ArtistInstrument.destroy_all
+Artist.destroy_all
+Instrument.destroy_all
 
-Instrument.create(name: "Guitar", classification: "Strings")
-Instrument.create(name: "Violin", classification: "Strings")
-Instrument.create(name: "Flute", classification: "Woodwind")
-Instrument.create(name: "Xylophone", classification: "Percussion")
+mj = Artist.create(name: "Michael Jackson", age: rand(50), title: "King of Pop")
+sw = Artist.create(name: "Stevie Wonder", age: rand(50), title: "Little Stevie Wonder")
+ep = Artist.create(name: "Elvis Presley", age: rand(50), title: "the King")
+wh = Artist.create(name: "Whitney Houston", age: rand(50), title: "The Voice")
+b = Artist.create(name: "Beyoncé", age: rand(50), title: "Destiny's Child")
+
+g = Instrument.create(name: "Guitar", classification: "Strings")
+v = Instrument.create(name: "Violin", classification: "Strings")
+f = Instrument.create(name: "Flute", classification: "Woodwind")
+x = Instrument.create(name: "Xylophone", classification: "Percussion")
+
+ArtistInstrument.create(artist: mj, instrument: g)
